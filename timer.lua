de4d023ecbe404b2b844b7bc5d4799c01f7fdb8b
Seniru Pasan Indira <senirupasan@gmail.com>
2019-10-05 11:51:43 +0530

    Basic support and other few options for timers
    Basic support include,
     - Create a timer
     - Kill a timer
     - Initialize timers
    
    Other options are,
     - Looping (the timers)
     - Call the callback with varargs
     - Resetting the timer

---
+++ timer.lua
@@ -0,0 +1,74 @@
+tfm.exec.newGame(0)
+
+local Timer = {}
+Timer.__index = Timer
+Timer._timers = {}
+Timer._init = false
+Timer._clock = 0
+
+setmetatable(Timer, {
+  __call = function (cls, ...)
+    return cls.new(...)
+  end,
+})
+
+function Timer.init(time) 
+  if not Timer._init then
+    Timer._init = true
+    Timer._clock = time
+  end
+end
+
+function Timer.process(tc)
+  Timer._clock = tc
+  for k, v in next, Timer._timers do
+    if v:isAlive() and v:getMatureTime() <= Timer._clock then
+      v.call(v.args)
+      if v.loop then
+        v:reset()
+      else
+        v:kill()
+      end
+    end
+  end
+end
+
+function Timer.run(tc)
+  Timer.init(tc)
+  Timer.process(tc)
+end
+
+function Timer.new(id, callback, timeout, loop, ...)
+  local self = setmetatable({}, Timer)
+  self.id = id
+  self.call = callback
+  self.timeout = timeout
+  self.mature = Timer._clock + timeout
+  self.loop = loop
+  self.args = ...
+  self.alive = true
+  Timer._timers[id] = self
+  return self
+end
+
+--[[Getters]]
+function Timer:getId() return self.id end
+function Timer:getMatureTime() return self.mature end
+function Timer:isAlive() return self.alive end
+
+function Timer:reset()
+  self.mature = Timer._clock + self.timeout
+end
+
+function Timer:kill()
+  self.alive = false
+  self = nil
+end
+
+--[[Testing]]
+
+local timer1 = Timer("timer1", function(name) print("Hello " .. name) end, 2000, true, "seniru")
+
+function eventLoop(tc, tr)
+  Timer.run(tc)
+end
