2e09dd252de51154faca01e7ef5c621f0ea02547
Seniru Pasan Indira <senirupasan@gmail.com>
2019-10-18 11:01:12 +0000

    Add automated doc tools an d changes in structure
    Changes in structure
     - Moved all the source code under the /src directory
     - Moved all the build tools under the /tools directory
     - Docs will be created inside /docs directory

--- tools/build.js
+++ tools/build.js
@@ -6,14 +6,14 @@ let code = '';
 function minifiy(code) {
     console.log('Minifying script...');
 
-    fs.writeFile("timer.min.lua", luamin.minify(code) + ";Timer=a\n", (err) => {
+    fs.writeFile("./src/timer.min.lua", luamin.minify(code) + ";Timer=a\n", (err) => {
         if (err) error(err);
     });
 }
 
 console.log('Reading timer.lua...');
 
-fs.readFile("timer.lua", "utf-8", (err, data) => {
+fs.readFile("./src/timer.lua", "utf-8", (err, data) => {
     code = data;
     minifiy(code);
     console.log("Successfully Written to File.");
