913c8032d247e418f141a200255ef5267a81e2db
Seniru Pasan Indira <senirupasan@gmail.com>
2019-10-27 12:18:19 +0000

    chore: Created bot to post new releases to the forum
    Added basic support to a bot that will post answers to library thread
    This bot is planned to use in CI on release events
    This bot was created using fromage API created by bolodefchoco

---
+++ tools/bot.lua
@@ -0,0 +1,23 @@
+local api = require("fromage")
+local client = api()
+local enums = client.enumerations()
+
+
+coroutine.wrap(function()
+    client.connect(args[2], args[3])
+
+    if client.isConnected() then
+        print('Successfully logged in as ' .. args[2])
+
+        print('Answering topic')
+
+        client.answerTopic("Testing fromage. Delete this message", {
+            f = enums.forum.transformice,
+            t = 885034
+        })
+
+    end
+
+    client.disconnect()
+    os.execute("pause >nul")
+end)()
