7980da37b1d261ee164cf28976a8cde240b9c27d
Seniru Pasan Indira <senirupasan@gmail.com>
2019-11-24 09:08:37 +0000

    chore: Completed the release notifier bot

--- tools/bot.lua
+++ tools/bot.lua
@@ -1,7 +1,8 @@
+local http = require('coro-http')
 local api = require("fromage")
+local json = require('json')
 local client = api()
 local enums = client.enumerations()
-
 
 coroutine.wrap(function()
     client.connect(args[2], args[3])
@@ -11,10 +12,24 @@ coroutine.wrap(function()
 
         print('Answering topic')
 
-        client.answerTopic("Testing fromage. Delete this message", {
-            f = enums.forum.atelier801,
-            t = 902724 --885034
+        local head, body = http.request('GET', 'https://api.github.com/repos/Seniru/Timers4TFM/releases/latest', {{ "user-agent", 'Seniru' }})
+        local data = json.parse(body)
+        
+        local msg = 
+        [[
+        [p=center][size=20][b]New Release! [i][color=#AAAAAA](]] .. data.tag_name .. [[)[/color][/i][/b][/size][/p]
+        [color=#CCCC22][size=18][b] ]] .. data.name .. [[ [/b][/size][/color]
+        
+        ]] .. data.body .. [[
+        
+        [hr][p=right][url=]] .. data.html_url .. [[]View on Github[/url][/p] ]]
+
+        client.answerTopic(msg, {
+            f = enums.forum.transformice,
+            t = 885034
         })
+
+        print('Answered successfully!')
 
     end
 
