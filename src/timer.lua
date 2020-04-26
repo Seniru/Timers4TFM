d8c0abc3e362b5cbc9c419291f3bf28e49147dc2
Seniru Pasan Indira <senirupasan@gmail.com>
2020-04-26 09:35:36 +0000

    chore: Use os.time instead of eventLoop ticks
    - use os.time instead of eventLoop ticks
    - delete stored timers after being utilised
    - remove unnecessary getters
    
    closes #30

--- src/timer.lua
+++ src/timer.lua
@@ -12,8 +12,6 @@
 local Timer = {}
 Timer.__index = Timer
 Timer._timers = {}
-Timer._init = false
-Timer._clock = 0
 
 setmetatable(Timer, {
     __call = function (cls, ...)
@@ -23,49 +21,30 @@ setmetatable(Timer, {
 
 --[====[
     @type func
-    @name Timer.init(time)
-    @param time:number Number of milliseconds from start
-    @brief Initializes all the timers and the clock
-    @desc Initializes all the timers and the clock. One should call this on each new game (otherwise the timers won't work properly. <br>The <i>time</i> could be passed inside the eventLoop function to be more precise.
---]====]
-function Timer.init(time)
-    if not Timer._init then
-        Timer._init = true
-        Timer._clock = time
-    end
-end
-
---[====[
-    @type func
     @name Timer.process(tc)
     @param tc:number Number of milliseconds from start
     @brief Processes all the timers
     @desc One should call this inside eventLoop function to work the timers properly
 --]====]
-function Timer.process(tc)
-    Timer._clock = tc
+function Timer.process()
+    local time = os.time()
+    local toRemove = {}
     for k, v in next, Timer._timers do
-        if v:isAlive() and v:getMatureTime() <= Timer._clock then
+        if v.isAlive and v.mature <= time then
             v:call()
             if v.loop then
                 v:reset()
             else
                 v:kill()
+                toRemove[#toRemove + 1] = k
             end
         end
     end
+    for k, v in next, toRemove do
+        Timer._timers[v] = nil
+    end
 end
 
---[====[
-    @type func
-    @name Timer.run(tc)
-    @param tc:number Number of milliseconds from start
-    @brief Shorthand method to include both #init and #process methods
---]====]
-function Timer.run(tc)
-    Timer.init(tc)
-    Timer.process(tc)
-end
 
 --[====[
     @type func
@@ -83,56 +62,14 @@ function Timer.new(id, callback, timeout, loop, ...)
     self.id = id
     self.callback = callback
     self.timeout = timeout
-    self.mature = Timer._clock + timeout
+    self.isAlive = true
+    self.mature = os.time() + timeout
     self.loop = loop
     self.args = { ... }
-    self.alive = true
     Timer._timers[id] = self
     return self
 end
 
---[[Getters]]
-
---[====[
-    @type func
-    @name Timer:getId()
-    @return any The timer's ID
-    @brief Returns the timer's ID
---]====]
-function Timer:getId() return self.id end
-
---[====[
-    @type func
-    @name Timer:getTimeout()
-    @return number Number of milliseconds for timeout
-    @brief Returns the timeout milliseconds
---]====]
-function Timer:getTimeout() return self.timeout end
-
---[====[
-    @type func
-    @name Timer:isLooping()
-    @return boolean The looping status
-    @brief Returns the looping status of the timer
---]====]
-function Timer:isLooping() return self.loop end
-
---[====[
-    @type func
-    @name Timer:getMatureTime()
-    @return number Time by which the timer get matured
-    @brief Returns the maturing time of the timer
---]====]
-function Timer:getMatureTime() return self.mature end
-
---[====[
-    @type func
-    @name Timer:isAlive()
-    @return boolean The timers living status
-    @brief Returns the timers life status
-    @desc This will return <i>true</i> if the timer is alive. A timer become dead if :kill() function is called on self. Also a timer get died after the mature if the loop status is <i>false</i>
---]====]
-function Timer:isAlive() return self.alive end
 
 --[[Setters]]
 
@@ -193,8 +130,7 @@ end
     @brief Kills the timer
 --]====]
 function Timer:kill()
-    self.alive = false
-    self = nil
+    self.isAlive = false
 end
 
 --[====[
@@ -204,5 +140,5 @@ end
     @desc Resets the timer. After reseting the mature time would be increased by the timeout provided at the instantiation
 --]====]
 function Timer:reset()
-    self.mature = Timer._clock + self.timeout
+    self.mature = os.time() + self.timeout
 end
